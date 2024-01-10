6b1f9fc2350e1cd5a062108d36a74a2db97e7608
Limit Liu <limitliu@qq.com>
2024-01-10 14:08:28 +0800

    fix: lspkind with cmp

--- lua/lsp/ui/init.lua
+++ lua/lsp/ui/init.lua
@@ -5,31 +5,44 @@ local M = {}
 
 local lspKindOk, lspkind = pcall(require, "lspkind")
 if lspKindOk then
-  local ok, _ = pcall(require, "nvim-cmp")
+  lspkind.init {}
+  local ok, _ = pcall(require, "cmp")
   local epoOk, _ = pcall(require, "epo")
+
+  local function get_symbol(kind)
+    local symbol = lspkind.symbol_map[kind]
+    local hasString = false
+    if symbol then
+      hasString = true
+    end
+    return hasString, symbol
+  end
 
   if ok then
     M.formatting = {
+      fields = { "kind", "abbr", "menu" },
       format = lspkind.cmp_format {
-        with_text = true,
+        mode = "text",
+        -- with_text = true,
         maxwidth = 50,
+        presets = "codicons",
         before = function(entry, vim_item)
-          vim_item.menu = "[" .. string.upper(entry.source.name) .. "]"
+          local kind = vim_item.kind
+          vim_item.menu = kind
+          local source = (
+            { nvim_lsp = "LSP", path = "Path", buffer = "Buffer" }
+          )[entry.source.name]
+          if source then
+            vim_item.menu = vim_item.menu .. " [" .. source .. "]"
+          end
+          vim_item.kind = lspkind.symbol_map[kind]
           return vim_item
         end,
       },
     }
   elseif epoOk then
     M.epo_formatting = function(kind)
-      local function get_symbol()
-        local symbol = lspkind.symbol_map[kind]
-        local hasString = false
-        if symbol then
-          hasString = true
-        end
-        return hasString, symbol
-      end
-      local o, s = get_symbol()
+      local o, s = get_symbol(kind)
       if o then
         return (s .. "  " .. kind)
       else
