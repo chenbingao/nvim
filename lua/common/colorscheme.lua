99d4c94ddf05e5272803923776573c9116cb9922
limit <limitliu@qq.com>
2024-01-21 00:29:51 +0800

    feat: use lua config

--- lua/common/colorscheme.lua
+++ lua/common/colorscheme.lua
@@ -1,20 +1,17 @@
+local config = require "config"
 vim.opt.termguicolors = true
 
-local parser = require "utils.parser"
-
 vim.schedule(function()
-  local _, json =
-    parser:parse(vim.api.nvim_list_runtime_paths()[1] .. "/config.json")
-  local color = json.colorscheme
+  local color = config.colorscheme
   vim.g.colors_name = color
-  -- vim.cmd("set background=" .. json.background)
-  vim.o.background = json.background or "dark"
+  -- vim.cmd("set background=" .. config.background)
+  vim.o.background = config.background or "dark"
   vim.opt.guifont = {
     -- vim.cmd [[
     -- set guifont=iMWritingMonoS\ Nerd\ Font:h16
     -- ]]
-    json.font or "Menlo Nerd Font Mono",
-    ":h" .. tostring(json.font_size or 16),
+    config.font or "Menlo Nerd Font Mono",
+    ":h" .. tostring(config.font_size or 16),
   }
-  vim.g.python3_host_prog = json.python3_host_prog or "/opt/local/bin/python3"
+  vim.g.python3_host_prog = config.python3_host_prog or "/opt/local/bin/python3"
 end)
