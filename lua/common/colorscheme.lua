ebab9ca8873e3300d1ae24ad951a6607f0cff749
Limit Liu <limitliu@qq.com>
2024-01-25 09:39:47 +0800

    chore: lua config file

--- lua/common/colorscheme.lua
+++ lua/common/colorscheme.lua
@@ -6,12 +6,12 @@ vim.schedule(function()
   vim.g.colors_name = color
   -- vim.cmd("set background=" .. config.background)
   vim.o.background = config.background or "dark"
-  vim.opt.guifont = {
-    -- vim.cmd [[
-    -- set guifont=iMWritingMonoS\ Nerd\ Font:h16
-    -- ]]
-    config.font or "Menlo Nerd Font Mono",
-    ":h" .. tostring(config.font_size or 16),
-  }
+  -- vim.opt.guifont = {
+  -- vim.cmd [[
+  -- set guifont=iMWritingMonoS\ Nerd\ Font:h16
+  -- ]]
+  -- config.font or "Menlo Nerd Font Mono",
+  -- ":h" .. tostring(config.font_size or 16),
+  -- }
   vim.g.python3_host_prog = config.python3_host_prog or "/opt/local/bin/python3"
 end)
