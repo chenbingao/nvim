1f1167ca76e20cd38a5a6d5ae9eb944b982c7390
Shiro <chenba@excellemed.com>
2024-03-07 13:38:27 +0800

    feat: migrate to mrcjkb/rustaceanvim

--- lua/common/colorscheme.lua
+++ lua/common/colorscheme.lua
@@ -2,16 +2,8 @@ local config = require "config"
 vim.opt.termguicolors = true
 
 vim.schedule(function()
-  local color = config.colorscheme
+  local color = config.colorscheme or "everforest"
   vim.g.colors_name = color
-  -- vim.cmd("set background=" .. config.background)
   vim.o.background = config.background or "dark"
-  -- vim.opt.guifont = {
-  -- vim.cmd [[
-  -- set guifont=iMWritingMonoS\ Nerd\ Font:h16
-  -- ]]
-  -- config.font or "Menlo Nerd Font Mono",
-  -- ":h" .. tostring(config.font_size or 16),
-  -- }
-  vim.g.python3_host_prog = config.python3_host_prog or "/opt/local/bin/python3"
+  vim.g.python3_host_prog = config.python3_host_prog or "/usr/bin/python3"
 end)
--- lua/lsp/dap/nvim-dap/rust.lua
+++ lua/lsp/dap/nvim-dap/rust.lua
@@ -1,9 +1,10 @@
-local extension_path = "~/.local/share/nvim/mason/packages/codelldb/extension"
+local extension_path = os.getenv "HOME"
+  .. "/.local/share/nvim/mason/packages/codelldb/extension"
 local codelldb_path = extension_path .. "/adapter/codelldb"
 local liblldb_path = extension_path .. "/lldb/lib/liblldb.dylib"
 
 return {
-  adapter = require("rust-tools.dap").get_codelldb_adapter(
+  adapter = require("rustaceanvim.config").get_codelldb_adapter(
     codelldb_path,
     liblldb_path
   ),
